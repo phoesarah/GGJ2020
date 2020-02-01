1535164dd573684e22b86eeb5d9116c78b6bd69f
jamjay <gamejamj@gmail.com>
2020-02-01 16:24:40 -0600

    Collision stuff

--- Godot/Scripts/Ball.gd
+++ Godot/Scripts/Ball.gd
@@ -1,13 +1,19 @@
 extends RigidBody
 signal hit_prop
+var glove_live: bool = 0
 
 func _ready():
-	self.connect("hit_prop", get_node("Player"), "hit_prop")
+	self.connect("hit_prop", get_node("../Player"), "ball_hit")
+	self.connect("body_entered", self, "body_entered")
 	pass
 
 #func _process(delta):
 	
 func body_entered(body):
-	if body.get_type() == PropP:
-		pass
+	print("Hit "+String(body.name))
+	if glove_live==false and body.name!="Glove":
+		glove_live = 1
+		return
+	if (body is PropP):
+		print("Hit propp")
 		emit_signal("hit_prop", body)
